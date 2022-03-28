e589e3607e1fff8589a8a8676f0f3d66317e3aac
Alexandre Arrieche De Conti <alexandre.conti@mjv.com.br>
2022-03-28 12:06:15 -0300

    Outras formas de trabalhar com delegates, utilizando expressões lambda.

--- Course/Program.cs
+++ Course/Program.cs
@@ -6,14 +6,22 @@ list.Add(new Product("TV", 900.00));
 list.Add(new Product("Notebook", 1200.00));
 list.Add(new Product("Tablet", 450.00));
 
-list.Sort(CompareProducts);
+//Comparison<Product> comp = CompareProducts;
+
+//Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+
+//list.Sort(CompareProducts);
+
+//list.Sort(comp);
+
+list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
 
 foreach (var p in list)
 {
     Console.WriteLine(p);
 }
 
-static int CompareProducts(Product p1, Product p2)
-{
-    return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-}
+//static int CompareProducts(Product p1, Product p2)
+//{
+//    return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+//}
