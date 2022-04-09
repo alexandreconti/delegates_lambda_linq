41a7f8703d1f99fadb113297810fda28344b57d2
Alexandre Arrieche De Conti <alexandre.conti@mjv.com.br>
2022-04-09 17:22:38 -0300

    Novo projeto implementações Linq.

---
+++ LinqImplementation/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello, World!");
+
+// Specify the data souce
+int[] numbers = new int[] { 2, 3, 4, 5 };
+
+// Define the query expression
+var result = numbers
+    .Where(x => x % 2 == 0)
+    .Select(x => x * 2);
+
+// Execute the query
+foreach (int number in result)
+{
+    Console.WriteLine(number);
+}
