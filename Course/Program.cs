e0bdb9a05ee7291ef83acc3af995ac1ca46b99e8
Alexandre Arrieche De Conti <alexandre.conti@mjv.com.br>
2022-05-07 12:17:30 -0300

    Implementações usando Linq part 2

--- Course/Program.cs
+++ Course/Program.cs
@@ -30,8 +30,6 @@ List<Product> products = new List<Product>() {
     new Product() {Id = 4, Name = "Level", Price = 70.0, Category = c1 }
 };
 
-//teste git hub
-
 var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
 Print("TIER 1 AND PRICE < 900:", r1);
 
@@ -49,7 +47,6 @@ Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4:", r5);
 
 var r6 = products.First();
 Console.WriteLine("First test1: " + r6);
-
 var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
 Console.WriteLine("First or defaul test2: " + r7);
 Console.WriteLine();
@@ -59,6 +56,34 @@ Console.WriteLine("Single or default test1: " + r8);
 
 var r9 = products.Where(p => p.Id == 30).SingleOrDefault();
 Console.WriteLine("Single or default test2: " + r9);
+Console.WriteLine();
+
+var r10 = products.Max(p => p.Price);
+Console.WriteLine("Max price: " + r10);
+var r11 = products.Min(p => p.Price);
+Console.WriteLine("Min price: " + r11);
+var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
+Console.WriteLine("Category 1 Sum prices: " + r12);
+var r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
+Console.WriteLine("Category 1 Average prices: " + r13);
+var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+Console.WriteLine("Category 5 Average prices: " + r14);
+//var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate((x, y) => x + y);
+//Console.WriteLine("Category 1 aggregate sum: " + r15);
+var r15 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
+Console.WriteLine("Category 1 aggregate sum: " + r15);
+Console.WriteLine();
+
+var r16 = products.GroupBy(p => p.Category);
+foreach(var group in r16)
+{
+    Console.WriteLine("Category" + group.Key.Name + ":");
+    foreach (var item in group)
+    {
+        Console.WriteLine(item);
+    }
+    Console.WriteLine();
+}
 
 #region exercicios delegates lambda
 //List<Product> list = new List<Product>();
